9c47acf746f068377f6710d17854772c9ba4694a
harishjartarghar <harish.jartarghar@gmail.com>
2020-11-25 22:38:56 +0530

    stepper css added

--- client/src/components/stepper.js
+++ client/src/components/stepper.js
@@ -1,49 +1,189 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Check from '@material-ui/icons/Check';
+import PasswordIcon from '@material-ui/icons/Lock';
+import InfoIcon from '@material-ui/icons/Info';
+import EmailIcon from '@material-ui/icons/Email';
+import StepConnector from '@material-ui/core/StepConnector';
+
+
+
+
+const useQontoStepIconStyles = makeStyles({
+  root: {
+    color: '#eaeaf0',
+    display: 'flex',
+    height: 10,
+    alignItems: 'center',
+  },
+  active: {
+    color: '#784af4',
+  },
+  circle: {
+    width: 8,
+    height: 8,
+    borderRadius: '50%',
+    backgroundColor: 'currentColor',
+  },
+  completed: {
+    color: '#784af4',
+    zIndex: 1,
+    fontSize: 18,
+  },
+});
+
+function QontoStepIcon(props) {
+  const classes = useQontoStepIconStyles();
+  const { active, completed } = props;
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+      })}
+    >
+      {completed ? <Check className={classes.completed} /> : <div className={classes.circle} />}
+    </div>
+  );
+}
+
+QontoStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   */
+  active: PropTypes.bool,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   */
+  completed: PropTypes.bool,
+};
+
+const ColorlibConnector = withStyles({
+  alternativeLabel: {
+    top: 22,
+  },
+  active: {
+    '& $line': {
+      backgroundImage:
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+    },
+  },
+  completed: {
+    '& $line': {
+      backgroundImage:
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+    },
+  },
+  line: {
+    height: 3,
+    border: 0,
+    backgroundColor: '#eaeaf0',
+    borderRadius: 1,
+  },
+})(StepConnector);
+
+const useColorlibStepIconStyles = makeStyles({
+  root: {
+    backgroundColor: '#ccc',
+    zIndex: 1,
+    color: '#fff',
+    width: 40,
+    height: 40,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  active: {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+  },
+  completed: {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+  },
+});
+
+function ColorlibStepIcon(props) {
+  const classes = useColorlibStepIconStyles();
+  const { active, completed } = props;
+
+  const icons = {
+    1: <EmailIcon />,
+    2: <InfoIcon />,
+    3: <PasswordIcon />,
+  };
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+        [classes.completed]: completed,
+      })}
+    >
+      {icons[String(props.icon)]}
+    </div>
+  );
+}
+
+ColorlibStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   */
+  active: PropTypes.bool,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   */
+  completed: PropTypes.bool,
+  /**
+   * The label displayed in the step icon.
+   */
+  icon: PropTypes.node,
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
+    margin:'auto',
+    marginTop:'10px',
+    border:'2px solid rgb(138,35,135)',
+
   },
-  backButton: {
+  button: {
     marginRight: theme.spacing(1),
   },
   instructions: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
-  }
+  },
 }));
 
 
-function getSteps() {
-  return ['FILL BASIC DETAILS', 'SET PASSWORD'];
-}
 
-
-
-
-export default function HorizontalLabelPositionBelowStepper({activeStep}) {
+export default function CustomizedSteppers({activeStep,steps}) {
   const classes = useStyles();
-  const steps = getSteps();
+
+
   
+
   return (
     <div className={classes.root}>
-      <Stepper classes={classes.stepper} activeStep={activeStep} alternativeLabel>
-	      <Step key="EMAIL VERIFIED">
-	            <StepLabel>EMAIL VERIFIED</StepLabel>
-	      </Step>
+      <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
+        <Step key="EMAIL VERIFIED">
+            <StepLabel StepIconComponent={ColorlibStepIcon}>EMAIL VERIFIED</StepLabel>
+          </Step>
         {steps.map((label) => (
           <Step key={label}>
-            <StepLabel>{label}</StepLabel>
+            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
           </Step>
         ))}
       </Stepper>
-   
     </div>
   );
 }
--- client/src/layouts/NgoRegister.js
+++ client/src/layouts/NgoRegister.js
@@ -1,1 +1,98 @@
-NgoRegister.js
+import React from "react";
+import {Redirect} from "react-router-dom";
+import Stepper from '../components/stepper';
+import NGODetailForm from '../components/NGODetailForm';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '../components/AppBar';
+
+
+class NgoRegister extends React.Component {
+
+constructor(props)
+{	
+	super(props);
+	this.state={
+		activeStep:1,
+    steps:['FILL BASIC DETAILS', 'SET PASSWORD']
+	}
+}
+
+useQuery =()=> {return new URLSearchParams(this.props.location.search);}
+
+getStepContent=(stepIndex)=> {
+  switch (stepIndex) {
+    case 0:
+      return 'EMAIL VERIFIED';
+    case 1:
+      return 'NGO DETAILS';
+    case 2:
+      return 'SET PASSWORD';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
+
+
+
+
+
+  handleNext = () => {
+    this.setState({activeStep:this.state.activeStep + 1});
+  };
+
+  handleBack = () => {
+  	this.setState({activeStep:this.state.activeStep==1?this.state.activeStep:this.state.activeStep - 1});
+  };
+
+  handleReset = () => {
+    this.setState({activeStep:1});
+  };
+
+render(){
+	let query = this.useQuery();
+
+	if(query.get("token")===undefined || query.get("token")===null || query.get("token")==="")
+		return <Redirect to="/login"/>;
+	
+	return (
+    <div className="donor">
+    <AppBar/>
+    <Container>
+   
+    <Stepper activeStep={this.state.activeStep} steps={this.state.steps}/>
+    <NGODetailForm/>
+  <div>
+        {this.state.activeStep === 3 ? (
+          <div style={{textAlign:"center"}}>
+            <Button onClick={this.handleReset}>Reset</Button>
+            <Button variant="contained" color="primary">Submit</Button>
+          </div>
+        ) : (
+          <div style={{textAlign:"center"}}>
+            
+            <div>
+              {this.state.activeStep===1?null:<Button
+                              disabled={this.state.activeStep === 0}
+                              onClick={this.handleBack}
+                              
+                            >Back
+              </Button>}
+                
+              <Button variant="contained" color="primary" onClick={this.handleNext}>
+                Next
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+      </Container>
+      </div>
+      );
+	
+	}
+  
+}
+
+export default NgoRegister;
